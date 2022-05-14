facef7bb3400a1bbc35cb89797ac3a84d0328bb8
omar.ali <ali582@avaya.com>
2022-05-14 15:19:25 +0200

    add filter component

---
+++ app/components/vue-components/filter-employees.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="c-filter-employees">
+    <input type="text"  v-model="filter"  class="c-filter-employees__input"/>
+    <button @click="clear" class="c-button">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+    return {
+      filter:"",
+    }
+  },
+  watch:{
+    filter:{
+      handler(value){
+        
+      }
+    }
+  },
+  methods:{
+    clear(){
+        this.filter = "";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
