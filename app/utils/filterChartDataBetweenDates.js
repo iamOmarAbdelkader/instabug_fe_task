0ac3e3a48130734b47727e07676e36d5c57a4019
omar.ali <ali582@avaya.com>
2022-05-14 21:56:25 +0200

    fix the filterChartDataBetweenDates bug

--- app/utils/filterChartDataBetweenDates.js
+++ app/utils/filterChartDataBetweenDates.js
@@ -1,5 +1,4 @@
 const filterChartDataBetweenDates = function (start, end, data) {
-  start = new Date(start - (start % 86400000));
   end = new Date(end - (end % 86400000) + 86400000);
   let res = [];
   data.forEach(item => {
