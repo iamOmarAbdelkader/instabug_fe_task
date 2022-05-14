3ef2754b6a83a73f20eff4929f741774553a4ab0
omar.ali <ali582@avaya.com>
2022-05-14 15:31:31 +0200

    create hightlight filter

--- app/components/employees-list/employees-list.component.js
+++ app/components/employees-list/employees-list.component.js
@@ -7,6 +7,13 @@ angular
     bindings: {
       employeesList: '<',
     },
+  }).filter('highlight', function ($sce) {
+    return function (text, phrase) {
+      if (phrase) {
+        text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlighted">$1</span>');
+      }
+      return $sce.trustAsHtml(text);
+    };
   });
 
 function EmployeesListComponent() {}
